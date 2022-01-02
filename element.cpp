0e0a5ff48200872d79f1ffd5688e84e578fdad52
Oleg <kombo007@mail.ru>
2022-01-03 00:59:37 +0700

    Модификация Element
    Добавлены методы get, set

--- element.cpp
+++ element.cpp
@@ -28,3 +28,57 @@ ostream& operator<< (ostream &out, const Element elem){
 
     return out;
 }
+
+string Element::getName(){
+    return name;
+}
+
+string Element::getStateStart(){
+    return stateStart;
+}
+
+string Element::getStateEnd(){
+    return stateEnd;
+}
+
+string Element::getCssClasses(){
+    return cssClasses;
+}
+
+string Element::getValue(){
+    return value;
+}
+
+int Element::setName(string nName){
+    if(nName == "" || nName == "\n" || nName[0] == ' '){
+        return 1;
+    }
+    name = nName;
+    return 0;
+}
+
+int Element::setStateStart(string nStateStart){
+    if(nStateStart == "" || nStateStart == "\n" || nStateStart[0] == ' '){
+        return 1;
+    }
+    stateStart = nStateStart;
+    return 0;
+}
+
+int Element::setStateEnd(string nStateEnd){
+    if(nStateEnd == "" || nStateEnd == "\n" || nStateEnd[0] == ' '){
+        return 1;
+    }
+    stateEnd = nStateEnd;
+    return 0;
+}
+
+int Element::setCssClasses(string nCssClasses){
+    cssClasses = nCssClasses;
+    return 0;
+}
+
+int Element::setValue(string nValue){
+    value = nValue;
+    return 0;
+}
--- element.h
+++ element.h
@@ -14,6 +14,18 @@ public:
     Element();
     Element(std::string name, std::string nStateStart, std::string nStateEnd, std::string nCssClasses, std::string nValue);
 
+    std::string getName();
+    std::string getStateStart();
+    std::string getStateEnd();
+    std::string getCssClasses();
+    std::string getValue();
+
+    int setName(std::string name);
+    int setStateStart(std::string stateStart);
+    int setStateEnd(std::string stateEnd);
+    int setCssClasses(std::string cssClasses);
+    int setValue(std::string value);
+
     friend std::ostream& operator<< (std::ostream &out, const Element elem);
 };
 
